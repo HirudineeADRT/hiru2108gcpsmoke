9a87c4d616fc123fef6a2305636bcd83e6d9007b
HirudineeADRT <hirudinee@adroitlogic.com>
2019-08-22 10:30:42 +0530

    test

--- index.js
+++ index.js
@@ -4,3 +4,4 @@
 exports.hiru2108gcpsmokefunction = require("./hiru2108gcpsmoke/function.js").handler;
 exports.hiru2108gcpsmokesamplehiru = require("./hiru2108gcpsmoke/samplehiru.js").handler;
 exports.hiru2108gcpsmoketestsamplehiru = require("./hiru2108gcpsmoke/testsamplehiru.js").handler;
+exports.hiru2108gcpsmoketesthiru77 = require("./hiru2108gcpsmoke/testhiru77.js").handler;
